efb0529e29e8feb02db053456becc8dd2125f724
SikeDomonkos <siked@kkszki.hu>
2024-10-21 09:02:45 +0200

    kesz a migration

---
+++ Blog/Models/BlogdbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Blog.Models
+{
+    public class BlogdbContext : DbContext
+    {
+        public BlogdbContext() { }
+        public BlogdbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string conn = "server=localhost; database=Blog; user=root; password=";
+
+                optionsBuilder.UseMySQL(conn);
+            }
+
+        }
+        public DbSet<Blogger> Bloggers { get; set; } = null;
+    }
+
+    
+}
