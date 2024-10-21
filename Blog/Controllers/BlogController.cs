b317d73baecdd186a98e8ba2fc53ed0803c2ae24
SikeDomonkos <siked@kkszki.hu>
2024-10-21 10:19:48 +0200

    kesz a controllerbe a httpgetid

---
+++ Blog/Controllers/BlogController.cs
@@ -0,0 +1,63 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.X509;
+using static Blog.Models.Dtocs;
+
+namespace Blog.Controllers
+{
+    [Route("blog")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Blogger> Get()
+        {
+            using (var context = new BlogdbContext())
+            {
+                return Ok(context.Bloggers.ToList());
+            }
+                
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Blogger> GetById(Guid id)
+        {
+            using (var context = new BlogdbContext())
+            {
+                var blogger = context.Bloggers.FirstOrDefault(x => x.Id == id);
+                if (blogger != null)
+                {
+                    return StatusCode(200, blogger);
+                } 
+                return NotFound();
+            }
+
+        }
+        [HttpPost]
+        public ActionResult<Blogger> Post(CreateBloggerDto createBloggerDto) 
+        {
+            using (var context = new BlogdbContext())
+            {
+                var blogger = new Blogger()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = createBloggerDto.Name,
+                    Sex = createBloggerDto.Sex,
+                    Status = "Waiting",
+                    RegistrationTime = DateTime.Now,
+                };
+
+                if (blogger != null)
+                {
+                    context.Bloggers.Add(blogger);
+                    context.SaveChanges();
+                    return StatusCode(201);
+                }
+            }
+
+            return Ok();
+        }
+    }
+
+    
+}
---
+++ Blog/Models/Dtocs.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog.Models
+{
+    public class Dtocs
+    {
+        public record CreateBloggerDto (string Name , string Sex);
+    }
+}
